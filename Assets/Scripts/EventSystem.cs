231f88f0effa2c0f7d5230817a81e18074e1ad94
Chui <lordbabypanda@gmail.com>
2015-10-08 15:43:25 -0500

    I finally to think.

--- Assets/Scripts/EventSystem.cs
+++ Assets/Scripts/EventSystem.cs
@@ -1,46 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
-static public class EventSystem
+public static class EventSystem
 {
-    static Dictionary<string, callBack> _event = new Dictionary<string, callBack>();
-    static List<object> subscribers = new List<object>();
+    static Dictionary<string, callBack> eventTable = new Dictionary<string, callBack>(); // Difference of Class of Delegate and delegate, callBack = delegate
 
-    public static void publisher(string message)
+    public static void Publisher(string message) // Publisher publishes a message, and the subscriber will subscriber to that message while I will exe the function.
     {
-        Debug.Log("Event: " + message);
-        callBack del;
-        foreach(object o in _event)
+       if(eventTable.ContainsKey(message))
         {
-            _event.TryGetValue(message, out del);
+            eventTable[message]();
         }
     }
 
-    public static bool subscriber(string message, callBack func)
+    public static void Subscriber(string key, callBack function)
     {
-        Subscriber subs = new Subscriber(message, func);
-        object obj = subs as object;
-        subscribers.Add(obj);
-
-        return true;
+        eventTable.Add(key, function);
     }
 
-
-    private class Subscriber
+    [ContextMenu("Subs")]
+    public static void Subs()
     {
-        private string m_message;
-        private callBack callback;
+        Subscriber("Message", InterestedSubs); // exe the function
+    }
 
-        public Subscriber(string message, callBack cb)
+    public static void InterestedSubs()
+    {
+        Debug.Log("It works!");
+    }
+
+    public static void RemoveSubscriber(string message, callBack function) // if two subscribers are on one message, multicast them together.
+    {
+        if(eventTable.TryGetValue(message, out function))
         {
-            m_message = message;
-            callback = cb;
+            eventTable[message] += function;
         }
-
-        public string Message { get { return m_message.ToLower(); } }
-        public callBack Method { get{ return callback; } }
-
     }
 
+    public static void test()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Publisher("Message");
+        }
+    }
 }
+
+/*
+Ok, Observer Patten. Like the fsm. Yup.
+So I know, there are 3 important things, Publisher, Subscriber, Messager.
+The Publisher will publish a message out whenever.
+the message is just a string.
+and the subscriber will subscribe to the message when the Publisher sends it out, it doesn't care "What" is in it, but by when, most likely when it's send out 
+by the string and use a delegate to exe that function.
+
+make a dictionary like this
+Dictionary PaulsEventTable<string, Delegate>
+|key        |value
+alpha       null
+bravo       null
+charlie     null
+
+    PaulsEventTable["alpha"] = what do you do? access it
+*/
+
+
+//static Dictionary<string, callBack> eventTable = new Dictionary<string, callBack>();
+
+//public delegate void EventHandler();
+
+//public static void addElement(string key, callBack T)
+//{
+//    eventTable.Add(key, null);
+//}
+
+//public static void Setup()
+//{
+//    addElement("Alpha", function);
+//    addElement("Bravo", function);
+//    addElement("Charlie", function);
+//    addElement("Delta", function);
+//    addElement("Foxtrot", function);
+//}
+
+//public static void function()
+//{
+//    Debug.Log("Function");
+//}
---
+++ Assets/Scripts/TranTask.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class TranTask
+{
+    public static void Start()
+    {
+        EventSystem.Subscriber("Message", Doit);
+    }
+
+    public static void Doit()
+    {
+        Debug.Log("Do it!!! :D");
+    }
+}
